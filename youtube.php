5a116cf0a5cf8ca99057e8695dc01c3a6dc2f957
bon0602 <108201524+bon0602@users.noreply.github.com>
2023-07-17 14:28:37 +0800

    Add files via upload

---
+++ youtube.php
@@ -0,0 +1,19 @@
+﻿<?php
+/**
+youtube LIVE: https://www.youtube.com/watch?v=lu_BJKxqGnk
+==> ?v=lu_BJKxqGnk &hd=0-7
+==> http://127.0.0.1/youtube.php?v=lu_BJKxqGnk&hd=5
+**/
+
+$v = $_GET['v'];
+$hd = $_GET['hd'];
+$data = file_get_contents('https://www.youtube.com/watch?v='.$v);
+preg_match('/"hlsManifestUrl":"(.*?)"/', $data, $matches);
+$data = file_get_contents($matches[1]);
+$ext = explode("\n", $data);
+unset($ext[0]);
+$ext = array_map('trim', $ext);
+$ext = array_chunk($ext, 2);
+header("Location:". $ext[$hd][0]);
+
+?>
